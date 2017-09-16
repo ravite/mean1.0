1368a3fe7f8cd9699c9ee7d375541e482edb7474
ravite <ravitejagodavarthi@gmail.com>
2017-09-16 13:15:42 +0530

    Delete db.js

--- db.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose');
-var connection = mongoose.connect('mongodb://localhost/admin');
- 
-//  console.log("Your connection is:",connection);
-module.exports = connection;
